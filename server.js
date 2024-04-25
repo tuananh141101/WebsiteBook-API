8dad54ababe073945af62e0f91bd9c87198dbd71
tuananh141101 <83608711+tuananh141101@users.noreply.github.com>
2024-04-25 15:03:33 +0700

    update

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
-const jsonServer = require('json-server')
-const express = require('express');
-const path = require('path');
+const jsonServer = require("json-server");
+const express = require("express");
+const path = require("path");
 
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use('/public', express.static(path.join(__dirname, 'public')))
+// Thêm middleware để cấu hình header 'Access-Control-Allow-Origin'
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+server.use(middlewares);
+server.use("/public", express.static(path.join(__dirname, "public")));
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
 
 // Serve homepage with links to all APIs
-server.get('/', (req, res) => {
-    const resources = Object.keys(router.db.__wrapped__)
-    const links = resources.map(resource => `<li><a href="/${resource}">${resource}</a></li>`)
-    res.send(`<h1>APIs:</h1><ul>${links.join('')}</ul>`)
-})
+server.get("/", (req, res) => {
+  const resources = Object.keys(router.db.__wrapped__);
+  const links = resources.map(
+    (resource) => `<li><a href="/${resource}">${resource}</a></li>`
+  );
+  res.send(`<h1>APIs:</h1><ul>${links.join("")}</ul>`);
+});
 
-server.use(router)
+server.use(router);
 server.listen(process.env.PORT || 3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
